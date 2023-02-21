c235327f952a3548635bc307f727887206bc5d7e
brian schafer <roycepdev@gmail.com>
2023-02-20 19:42:35 -0800

    added random placement of boxes

--- src/components/LanguageBoxes.jsx
+++ src/components/LanguageBoxes.jsx
@@ -53,6 +53,24 @@ export default function LanguageBoxes() {
     "metamask.png",
   ]);
 
+  const boxes = [
+    { name: javascript, map: javascriptColor },
+    { name: html, map: htmlColor },
+    { name: css, map: cssColor },
+    { name: typescript, map: typescriptColor },
+    { name: react, map: reactColor },
+    { name: next, map: nextColor },
+    { name: vercel, map: vercelColor },
+    { name: styledComponents, map: styledComponentsColor },
+    { name: tailwind, map: tailwindColor },
+    { name: github, map: githubColor },
+    { name: figma, map: figmaColor },
+    { name: three, map: threeColor },
+    { name: supabase, map: supabaseColor },
+    { name: rainbowkit, map: rainbowkitColor },
+    { name: metamask, map: metamaskColor },
+  ];
+
   //       // A one-off "push"
   //       javascript.current.applyImpulse({ x: 0, y: 1, z: 0 }, true);
 
@@ -67,103 +85,27 @@ export default function LanguageBoxes() {
 
   const handleClick = (box) => {
     // A one-off torque rotation
-    box.current.applyTorqueImpulse({ x: 0.1, y: 0.3, z: 0.2 }, true);
+    box.current.applyTorqueImpulse({ x: 0.05, y: 0.1, z: 0.05 }, true);
     // A one-off "push"
     box.current.applyImpulse({ x: 0, y: 0.2, z: -0.2 }, true);
   };
 
   return (
     <>
-      <RigidBody onClick={() => handleClick(javascript)} ref={javascript}>
-        <mesh position={[0, 2, 2.5]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={javascriptColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(html)} ref={html}>
-        <mesh position={[-1.15, 2, 2.2]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={htmlColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(css)} ref={css}>
-        <mesh position={[1.2, 2, 2]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={cssColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(typescript)} ref={typescript}>
-        <mesh position={[1, 2, 1.75]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={typescriptColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(react)} ref={react}>
-        <mesh position={[1.15, 3, 1.75]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={reactColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(next)} ref={next}>
-        <mesh position={[-1.25, 3, 2.15]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={nextColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(vercel)} ref={vercel}>
-        <mesh position={[-1.75, 4, 2.25]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={vercelColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(styledComponents)} ref={styledComponents}>
-        <mesh position={[-1.75, 3, 1.75]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={styledComponentsColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(tailwind)} ref={tailwind}>
-        <mesh position={[1.5, 3, 2]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={tailwindColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(github)} ref={github}>
-        <mesh position={[1.75, 4, 2.15]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={githubColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody ref={figma} onClick={() => handleClick(figma)}>
-        <mesh position={[1.75, 5, 1.75]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={figmaColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(three)} ref={three}>
-        <mesh position={[0.75, 5, 2.25]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={threeColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(supabase)} ref={supabase}>
-        <mesh position={[0.25, 5, 1.75]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={supabaseColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(rainbowkit)} ref={rainbowkit}>
-        <mesh position={[-1.15, 5, 2]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={rainbowkitColor} />
-        </mesh>
-      </RigidBody>
-      <RigidBody onClick={() => handleClick(metamask)} ref={metamask}>
-        <mesh position={[-2.15, 5, 2]}>
-          <boxGeometry args={[0.5, 0.5, 0.5]} />
-          <meshStandardMaterial map={metamaskColor} />
-        </mesh>
-      </RigidBody>
+      {boxes.map((box) => (
+        <RigidBody onClick={() => handleClick(box.name)} ref={box.name}>
+          <mesh
+            position={[
+              Math.random() * 7 - 3,
+              Math.random() * (5 - 3) + 3,
+              Math.random() * (-1 - -3 + 1) + -1.5,
+            ]}
+          >
+            <boxGeometry args={[0.5, 0.5, 0.5]} />
+            <meshStandardMaterial map={box.map} />
+          </mesh>
+        </RigidBody>
+      ))}
     </>
   );
 }
