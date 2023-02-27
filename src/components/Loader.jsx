d7a2ef78518f2c64a8ef362e2920ad0125bb72dc
brian schafer <roycepdev@gmail.com>
2023-02-26 22:10:02 -0800

    adjusted color on loading screen

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,9 +3,9 @@ import Styles from "../styles/loader.module.css";
 export default function Loader({ children, loadingProgress }) {
   return (
     <>
-  <div  className={Styles.bottomContainer}>
-    <div className={Styles.bottomLoadingTitle}>Loading</div>
-    <div className={Styles.bottomLoadingPercent}>{children}</div>
+  <div  className={Styles.container}>
+    <div className={Styles.title}>Loading</div>
+    <div className={Styles.subtitle}>{children}</div>
   </div>
   </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,6 @@ export default function Home() {
     setShowLoader(false);
   };
 
-
-
   return (
     <>
       <Head>
